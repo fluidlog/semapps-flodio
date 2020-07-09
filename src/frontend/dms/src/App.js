06ab1640bfa4ca4b714f6e1973d9d5341dbc23f5
srosset81 <srosset81@gmail.com>
2020-07-09 09:55:43 +0200

    DMS with working custom store

--- src/frontend/dms/src/App.js
+++ src/frontend/dms/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { createHashHistory } from 'history';
 import { Admin, Resource } from 'react-admin';
-import { dataProvider, authProvider, httpClient } from '@semapps/react-admin';
+import { dataProvider as ldpProvider, authProvider as createAuthProvider, httpClient } from '@semapps/react-admin';
 import LogoutButton from './auth/LogoutButton';
 import { ProjectList, ProjectEdit, ProjectCreate, ProjectIcon } from './resources/projects';
 import { OrganizationList, OrganizationEdit, OrganizationCreate, OrganizationIcon } from './resources/organizations';
@@ -9,38 +11,53 @@ import { ConceptList, ConceptIcon } from './resources/concepts';
 import resources from './config/resources';
 import ontologies from './config/ontologies';
 
+import createAdminStore from './createAdminStore';
+const history = createHashHistory();
+
+const dataProvider = ldpProvider({
+  sparqlEndpoint: process.env.REACT_APP_MIDDLEWARE_URL + 'sparql',
+  httpClient,
+  resources,
+  ontologies
+});
+const authProvider = createAuthProvider(process.env.REACT_APP_MIDDLEWARE_URL);
+
 function App() {
   return (
-    <Admin
-      dataProvider={dataProvider({
-        sparqlEndpoint: process.env.REACT_APP_MIDDLEWARE_URL + 'sparql',
-        httpClient,
-        resources,
-        ontologies
+    <Provider
+      store={createAdminStore({
+        authProvider,
+        dataProvider,
+        history,
       })}
-      authProvider={authProvider(process.env.REACT_APP_MIDDLEWARE_URL)}
-      logoutButton={LogoutButton}
     >
-      <Resource
-        name="Project"
-        list={ProjectList}
-        edit={ProjectEdit}
-        create={ProjectCreate}
-        icon={ProjectIcon}
-        options={{ label: 'Projets' }}
-      />
-      <Resource
-        name="Organization"
-        list={OrganizationList}
-        edit={OrganizationEdit}
-        create={OrganizationCreate}
-        icon={OrganizationIcon}
-        options={{ label: 'Organisations' }}
-      />
-      <Resource name="Person" list={PersonList} icon={PersonIcon} options={{ label: 'Contributeurs' }} />
-      <Resource name="Concept" list={ConceptList} icon={ConceptIcon} options={{ label: 'Concepts' }} />
-      <Resource name="Agent" />
-    </Admin>
+      <Admin
+        dataProvider={dataProvider}
+        authProvider={authProvider}
+        history={history}
+        logoutButton={LogoutButton}
+      >
+        <Resource
+          name="Project"
+          list={ProjectList}
+          edit={ProjectEdit}
+          create={ProjectCreate}
+          icon={ProjectIcon}
+          options={{ label: 'Projets' }}
+        />
+        <Resource
+          name="Organization"
+          list={OrganizationList}
+          edit={OrganizationEdit}
+          create={OrganizationCreate}
+          icon={OrganizationIcon}
+          options={{ label: 'Organisations' }}
+        />
+        <Resource name="Person" list={PersonList} icon={PersonIcon} options={{ label: 'Contributeurs' }} />
+        <Resource name="Concept" list={ConceptList} icon={ConceptIcon} options={{ label: 'Concepts' }} />
+        <Resource name="Agent" />
+      </Admin>
+    </Provider>
   );
 }
 
